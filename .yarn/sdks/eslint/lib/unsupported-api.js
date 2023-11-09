18c01c2f52ccee9c7d4b7307c4c465b7190c26a7
weareoutman <wangshenwei@qq.com>
2023-11-09 09:32:27 +0800

    feat: support docusaurus v3

--- .yarn/sdks/eslint/lib/unsupported-api.js
+++ .yarn/sdks/eslint/lib/unsupported-api.js
@@ -4,17 +4,17 @@ const { existsSync } = require(`fs`);
 const { createRequire } = require(`module`);
 const { resolve } = require(`path`);
 
-const relPnpApiPath = "../../../.pnp.cjs";
+const relPnpApiPath = "../../../../.pnp.cjs";
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = createRequire(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
-    // Setup the environment to be able to require prettier
+    // Setup the environment to be able to require eslint/use-at-your-own-risk
     require(absPnpApiPath).setup();
   }
 }
 
-// Defer to the real prettier your application uses
-module.exports = absRequire(`prettier`);
+// Defer to the real eslint/use-at-your-own-risk your application uses
+module.exports = absRequire(`eslint/use-at-your-own-risk`);
